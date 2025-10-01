3773183586b24cb10cbbc1fabb2db6db5bdb36dc
Yenuli Minsadi <samararatney@gmail.com>
2025-10-01 11:20:12 +0530

    thread priority

--- intro_threads/06-Thread-Priority/src/main/java/lk/threads/Main.java
+++ intro_threads/06-Thread-Priority/src/main/java/lk/threads/Main.java
@@ -4,7 +4,30 @@ package lk.threads;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Thread t1 = new Thread();
-        Thread t2 = new Thread();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i<10; i++) {
+                System.out.println("Thread1 is running"+Thread.currentThread().getPriority());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i<10; i++) {
+                System.out.println("Thread2 is running"+Thread.currentThread().getPriority());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t1.start();
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.start();
+        t2.setPriority(Thread.MIN_PRIORITY);
     }
 }
